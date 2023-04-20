2147c6ef4ac683e24da24c2b28005f46f4f9039f
AceyTracy <werutracy@gmail.com>
2023-04-20 16:00:44 +0000

    tasks

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,20 @@
-# puppet manifest creating a custom HTTP header response
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+# Automate the creation of a custom HTTP header response with Puppet.
+
+exec { 'apt-get update':
+  command  => 'apt-get update',
+  provider => shell,
 }
 
 package { 'nginx':
   ensure  => installed,
-  require => Exec['apt-get-update'],
+  require => Exec['apt-get update'],
 }
 
-file_line { 'a':
+file_line { 'Header response':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'b':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
+  after   => 'server_name _;',
   line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
   require => Package['nginx'],
 }
 
